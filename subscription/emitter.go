fafccddfcfa37a289d85aeabcd243d5b87a0dc4c
mikezzb <mikezhoudev@gmail.com>
2024-04-18 14:30:34 -0400

    feat: change stream prototype

--- subscription/emitter.go
+++ subscription/emitter.go
@@ -1,11 +1,14 @@
 package subscription
 
 import (
+	"context"
 	"log"
 	"strconv"
 
 	"github.com/mikezzb/steam-trading-shared/database/model"
 	"github.com/mikezzb/steam-trading-shared/database/repository"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Event emitter pattern
@@ -19,6 +22,9 @@ type NotificationEmitter struct {
 	itemRaritySubs map[string][]*ParsedSubscription
 	// item name -> min price of item
 	itemPrices map[string]float64
+
+	// change streams
+	subChangeStream *mongo.ChangeStream
 }
 
 func NewNotificationEmitter(subRepo *repository.SubscriptionRepository, itemRepo *repository.ItemRepository, config *NotifierConfig) *NotificationEmitter {
@@ -43,8 +49,7 @@ func (e *NotificationEmitter) init() {
 
 	// group subscriptions by item name
 	for _, sub := range subs {
-		key := getItemRarityKey(sub.Name, sub.Rarity)
-		e.itemRaritySubs[key] = append(e.itemRaritySubs[key], GetParsedSubscription(&sub))
+		e.addSub(&sub)
 	}
 
 	// get all items
@@ -59,6 +64,68 @@ func (e *NotificationEmitter) init() {
 		priceFloat, _ := strconv.ParseFloat(item.LowestMarketPrice, 64)
 		e.itemPrices[item.Name] = priceFloat
 	}
+
+	// start monitoring
+	e.initMonitoring()
+}
+
+func (e *NotificationEmitter) addSub(sub *model.Subscription) {
+	key := getItemRarityKey(sub.Name, sub.Rarity)
+	e.itemRaritySubs[key] = append(e.itemRaritySubs[key], GetParsedSubscription(sub))
+}
+
+func (e *NotificationEmitter) initMonitoring() {
+	// monitor any update or insert to the subscription collection
+	changeStreamOpts := options.ChangeStream().SetFullDocument(options.UpdateLookup)
+	changeStream, err := e.subRepo.SubCol.Watch(context.Background(), mongo.Pipeline{}, changeStreamOpts)
+	if err != nil {
+		log.Fatalf("NotificationEmitter.initMonitoring: %v", err)
+		return
+	}
+	e.subChangeStream = changeStream
+
+	go e.monitorSubChanges()
+}
+
+func (e *NotificationEmitter) monitorSubChanges() {
+	defer e.subChangeStream.Close(context.Background())
+
+	// loop to monitor changes
+	for e.subChangeStream.Next(context.Background()) {
+
+		var subChange struct {
+			FullDocument  model.Subscription `bson:"fullDocument"`
+			OperationType string             `bson:"operationType"`
+		}
+		if err := e.subChangeStream.Decode(&subChange); err != nil {
+			log.Fatalf("NotificationEmitter.monitorSubChanges: %v", err)
+			return
+		}
+
+		switch subChange.OperationType {
+		case "delete":
+			// remove the subscription
+		case "update":
+			// update the subscription
+		case "insert":
+			// add the subscription
+			e.addSub(&subChange.FullDocument)
+		default:
+			log.Fatalf("NotificationEmitter.monitorSubChanges: unknown operation type %v", subChange.OperationType)
+		}
+
+		// update the subscription
+		key := getItemRarityKey(subChange.FullDocument.Name, subChange.FullDocument.Rarity)
+
+		//
+
+		e.itemRaritySubs[key] = append(e.itemRaritySubs[key], GetParsedSubscription(&subChange.FullDocument))
+	}
+
+	if err := e.subChangeStream.Err(); err != nil {
+		log.Fatalf("NotificationEmitter.monitorSubChanges: %v", err)
+		return
+	}
 }
 
 func (e *NotificationEmitter) EmitListing(listing *model.Listing) {
@@ -71,7 +138,7 @@ func (e *NotificationEmitter) EmitListing(listing *model.Listing) {
 		if e.IsPriceMatch(listing.Price, sub) {
 			// notify user
 			if listingMessage == "" {
-				listingMessage = GetListingMessage(listing)
+				listingMessage = GetListingMessage(listing, e.itemPrices[listing.Name])
 			}
 			e.notifer.Notify(sub.Subscription.NotiType, sub.Subscription.NotiId, listingMessage)
 		}
